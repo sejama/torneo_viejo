f9f8a300eacf5b9ffc5ab57f1e36baf711b99289
Sebastian Javier Maidana <seba_maidana_3@hotmail.com>
2024-05-16 15:36:17 -0300

    test

---
+++ entorno/www/mvc/tests/Unit/Manager/TorneoManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Manager;
+
+use PHPUnit\Framework\TestCase;
+
+class TorneoManagerTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
